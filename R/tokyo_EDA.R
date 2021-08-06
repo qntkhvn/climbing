b9fc5d21acf9f294ce3f8b8558bb8df65a82ca23
Quang Nguyen <ngminhquang.99@gmail.com>
2021-08-06 17:29:14 -0400

    Clean up Tokyo scripts

--- R/tokyo_EDA.R
+++ R/tokyo_EDA.R
@@ -1,77 +1,4 @@
-
-  
-# DATA scraping
-
-
-library(tidyverse)
-library(rvest)
-library(janitor)
-
-
-men_url <- "https://en.wikipedia.org/wiki/Sport_climbing_at_the_2020_Summer_Olympics_%E2%80%93_Men%27s"
-
-mq <- men_url %>%
-  read_html() %>%
-  html_table() %>%
-  .[[7]] %>%
-  row_to_names(row_number = 1) %>%
-  clean_names() %>% 
-  rename(overall = rank, 
-         speed = cp,
-         bouldering = cp_2,
-         lead = cp_3,
-         total = total_11)
-  
-
-mf <- men_url %>%
-  read_html() %>%
-  html_table() %>%
-  .[[8]] %>%
-  row_to_names(row_number = 1) %>%
-  clean_names() %>% 
-  filter(climber != "Climber") %>% 
-  mutate(rank = 1:8) %>% 
-  rename(overall = rank, 
-         speed = cp,
-         bouldering = cp_2,
-         lead = cp_3,
-         total = total_16)
-
-women_url <- "https://en.wikipedia.org/wiki/Sport_climbing_at_the_2020_Summer_Olympics_%E2%80%93_Women%27s_combined"
-
-wq <- women_url %>% 
-  read_html() %>% 
-  html_table() %>% 
-  .[[7]] %>% 
-  row_to_names(row_number = 1) %>% 
-  clean_names() %>% 
-  rename(overall = rank, 
-         speed = cp,
-         bouldering = cp_2,
-         lead = cp_3,
-         total = total_10)
-
-wf <- women_url %>% 
-  read_html() %>% 
-  html_table() %>% 
-  .[[8]] %>% 
-  row_to_names(row_number = 1) %>% 
-  clean_names() %>% 
-  filter(climber != "Climber") %>% 
-  mutate(rank = 1:8) %>% 
-  rename(overall = rank, 
-         speed = cp,
-         bouldering = cp_2,
-         lead = cp_3)
-
-
-write_csv(mf, "data/2020_olympics/mf.csv")
-
-
-
-
-#####
-
+# load data from data folder first
 
 mq %>% 
   select(climber,
@@ -198,7 +125,7 @@ wf %>%
           axisLabels = "none",
           upper = list(continuous = wrap("cor", method = "kendall", stars = FALSE))) +
   theme_light() + ggtitle("2020 Olympics - Women's Qualification")
-  
+
 
 rerank <- list()
 for (i in 1:nrow(df)) {
